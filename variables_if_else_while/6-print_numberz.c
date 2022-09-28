128b9080dbabd3c54459b5b59e17db9dc0d95907
juanivalle <juanignaciovalle18@gmail.com>
2022-09-28 10:37:17 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - task5
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int wii;
+
+	for (wii = '0'; wii <= '9'; wii++)
+		putchar (wii);
+	putchar ('\n');
+	return (0);
+}
