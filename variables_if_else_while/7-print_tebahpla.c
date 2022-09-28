af26a2fb08562e6bc0e772dae142eec21efa13b2
juanivalle <juanignaciovalle18@gmail.com>
2022-09-28 10:43:16 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - task5
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int Wii;
+
+	for (Wii = 'z'; Wii >= 'a'; Wii--)
+		putchar (Wii);
+	putchar ('\n');
+	return (0);
+}
