a48c1de728ad6dfd0c3ea5905193d264e8605bc5
juanivalle <juanignaciovalle18@gmail.com>
2022-09-28 09:50:05 -0700

    task3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - task3
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int Xd;
+
+	for (Xd = 'a'; Xd <= 'z'; Xd++)
+		putchar (Xd);
+	for (Xd = 'A'; Xd <= 'Z'; Xd++)
+		putchar (Xd);
+	putchar ('\n');
+	return (0);
+}	
