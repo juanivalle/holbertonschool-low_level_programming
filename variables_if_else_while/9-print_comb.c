15d799b79275f0ee4cce11be6079a71de10899b3
juanivalle <juanignaciovalle18@gmail.com>
2022-09-29 05:55:44 -0700

    task9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - task9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int boo;
+
+	for (boo = '0'; boo <= '8'; boo++)
+	{
+		putchar (boo);
+		if (boo < '9')
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
