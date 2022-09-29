49fa54bb9bb72aaa270ce651586af6ea6eedec24
juanivalle <juanignaciovalle18@gmail.com>
2022-09-29 07:18:43 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - task1
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar (x);
+	_putchar ('\n');
+}
