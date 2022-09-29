573a722d96606d327e336be3626212001bbd8320
juanivalle <juanignaciovalle18@gmail.com>
2022-09-29 06:54:32 -0700

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - task0
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int x;
+
+	for (x = '0'; str[x] != '\0'; x++)
+		_putchar (str[x]);
+	return (0);
+}
