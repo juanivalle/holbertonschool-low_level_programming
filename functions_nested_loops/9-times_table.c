fae219e50ec96fd03258cb5cfc1b7bc5008ff59b
juanivalle <juanignaciovalle18@gmail.com>
2022-10-03 06:09:45 -0700

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,21 +15,24 @@ void times_table(void)
 		{
 			int g = n * m;
 
-			if (g / 10 == 0)
+			if (m == 0)
 			{
-				_putchar (g % 10 + '0');
+				_putchar (g + '0');
+			}
+			else if (g / 10 == 0)
+			{
 				_putchar (',');
 				_putchar (' ');
 				_putchar (' ');
+				_putchar (g % 10 + '0');
 			}
 			else
 			{
-				_putchar (g / 10 + '0');
-				_putchar (g % 10 + '0');
 				_putchar (',');
 				_putchar (' ');
+				_putchar (g / 10 + '0');
+				_putchar (g % 10 + '0');
 			}
-
 		}
 	_putchar ('\n');
 	}
