8ad8fb1903db14cf8c22209e1b00e3b796e89f7a
juanivalle <juanignaciovalle18@gmail.com>
2022-10-04 08:38:38 -0700

    task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - task6
+ * @n: int
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = '0'; a <= n; a++)
+			_putchar ('_');
+	_putchar ('\n');
+	}
+	else
+		_putchar ('\n');
+}
