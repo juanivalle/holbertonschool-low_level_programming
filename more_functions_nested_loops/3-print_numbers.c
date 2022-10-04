a166f78412097adba219023b30bc44b9afc82e9e
juanivalle <juanignaciovalle18@gmail.com>
2022-10-04 06:39:51 -0700

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - task3
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+}
