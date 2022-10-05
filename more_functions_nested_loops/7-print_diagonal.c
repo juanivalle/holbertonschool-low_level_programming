c7d052884151e63c451ee794ed67b8362726f8c4
juanivalle <juanignaciovalle18@gmail.com>
2022-10-05 07:11:31 -0700

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - task7
+ * @n: int
+ * Return: always 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b != a; b++)
+				_putchar (' ');
+			_putchar ('\\');
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar ('\n');
+}
