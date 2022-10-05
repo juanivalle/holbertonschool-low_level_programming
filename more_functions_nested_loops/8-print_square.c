830da6ff757e50831052ab71547442c604ff1d2c
juanivalle <juanignaciovalle18@gmail.com>
2022-10-05 07:31:29 -0700

    task8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - task8
+ * @size: int
+ * Return: always 0
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar ('#');
+					_putchar ('\n');
+		}
+	}
+	else
+		_putchar ('\n');
+}
