30b82cd404907429ca480e1d5a862def3ac9690c
juanivalle <juanignaciovalle18@gmail.com>
2022-10-05 10:42:32 -0700

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - task10
+ * @size: int
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int a;
+	int space;
+	int numeral;
+
+	if (size <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (space = a; space < size; space++)
+			{
+				_putchar (' ');
+			}
+			for (numeral = a; numeral >= 0; numeral--)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	}
+}
