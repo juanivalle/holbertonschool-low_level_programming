a9cb4a1e92d34898c41166528ce19c1d9d83fdea
juanivalle <juanignaciovalle18@gmail.com>
2022-10-07 06:37:55 -0700

    task5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,19 @@
  */
 void rev_string(char *s)
 {
-	int l, a;
+	int a;
+	int b;
+	int c;
 
-	scanf("%c", s);
-	l = strlen(s);
-	for (a = l - 1; a >= 0; a--)
-	{}
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	a--;
+		for (b = 0; b < a; b++)
+		{
+			c = s[b];
+			s[b] = s[a];
+			s[a] = c;
+			a--;
+		}
 }
