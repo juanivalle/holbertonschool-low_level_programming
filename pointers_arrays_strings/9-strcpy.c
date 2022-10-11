3824387c5de3e9f20a549d6f04fbb888d4f1f94a
juanivalle <juanignaciovalle18@gmail.com>
2022-10-11 07:58:33 -0700

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy - task9
+ * @dest: char
+ * @src: char
+ * Return: always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	return (dest);
+}
