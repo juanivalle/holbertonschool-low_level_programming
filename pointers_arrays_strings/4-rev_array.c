defeca97dcafbf4db28834ef8539b217d7f155ad
juanivalle <juanignaciovalle18@gmail.com>
2022-10-12 10:24:30 -0700

    task4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - task4
+ * @a: int
+ * @n: int
+ * Return: always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int b;
+        int c;
+
+        for (b = 0; b < n / 2; b++)
+	{
+		b--;
+	}
+	c = a[b];
+	a[b] = a[n];
+	a[n] = c;
+	b--;
+}
