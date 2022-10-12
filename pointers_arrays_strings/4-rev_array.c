825656b44e0c2f61c17ea95fd8b8fcdcb1d19405
juanivalle <juanignaciovalle18@gmail.com>
2022-10-12 10:59:50 -0700

    task4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,8 +10,10 @@ void reverse_array(int *a, int n)
 	int b;
 	int c;
 
-	for (b = 0; b < n / 2; n++)
-	c = a[b];
-	a[b] = a[n - b - 1];
-	a[n - b - 1] = c;
+	for (b = 0; b < n / 2; b++)
+	{
+		c = a[b];
+		a[b] = a[n - b - 1];
+		a[n - b - 1] = c;
+	}
 }
