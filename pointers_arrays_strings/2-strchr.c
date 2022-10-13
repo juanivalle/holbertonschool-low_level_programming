9cb7b6ffb3f750340777b818803202427b3e1f5a
juanivalle <juanignaciovalle18@gmail.com>
2022-10-13 11:04:47 -0700

    task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - task2
+ * @s: char
+ * @c: char
+ * Return: always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			return (&s[a]);
+		}
+	}
+	return ('\0');
+}
