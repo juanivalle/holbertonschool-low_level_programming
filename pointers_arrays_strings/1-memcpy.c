9e218dfe5b05ee52e35b341087a42ce52b8a985a
juanivalle <juanignaciovalle18@gmail.com>
2022-10-13 10:45:31 -0700

    task1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_memcpy - task1
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: always 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int b;
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	for (; b < n; b++)
+	{
+		dest[b] = '\0';
+	}
+	return (dest);
+}
