0719edb96d25c1df9f0fe315ac14a026ae5e90ab
juanivalle <juanignaciovalle18@gmail.com>
2022-10-17 07:07:20 -0700

    task3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - task3
+ * @n: int
+ * Return: always 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
