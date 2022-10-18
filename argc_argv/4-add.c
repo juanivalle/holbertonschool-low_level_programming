d122bbc8c74015d91dc8472cb010caf957840d28
juanivalle <juanignaciovalle18@gmail.com>
2022-10-18 15:12:37 -0700

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - task4
+ * @argc: int
+ * @argv: char
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int suma;
+
+	suma = 0;
+	for (x = 0; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	suma += atoi(argv[x]);
+	}
+	printf("%d\n", suma);
+	return (0);
+}
