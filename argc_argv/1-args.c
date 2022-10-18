3520120a052c8d9ca2a3f0390294a3b76ed73823
juanivalle <juanignaciovalle18@gmail.com>
2022-10-18 07:29:39 -0700

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - task1
+ *@argc: int
+ *@argv: char
+ * Return: always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
