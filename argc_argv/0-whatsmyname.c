23192a506ade31c21fb8df321da5dc58d215687e
juanivalle <juanignaciovalle18@gmail.com>
2022-10-18 07:04:04 -0700

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - task0
+ *@argc: int
+ *@argv: char
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s,'\n'", argv[0]);
+	}
+	return (0);
+}
