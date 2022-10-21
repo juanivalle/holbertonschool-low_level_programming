4cafb4774fc60ce3236b849a6a8bd9275dd9c237
juanivalle <juanignaciovalle18@gmail.com>
2022-10-20 18:18:20 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -25,15 +25,10 @@ char *str_concat(char *s1, char *s2)
 	if (s3 != NULL && s4 != NULL)
 	{
 		for (a = 0; a <= strlen(s1) + 1; a++)
-		{
 			s3[a] = s1[a];
-		}
 		for (b = 0; b <= strlen(s2) ; b++)
-		{
 			s4[b] = s2[b];
-		}
 		s3 = strcat(s3, s4);
-
 		{
 		if (s3 == NULL)
 		return (NULL);
