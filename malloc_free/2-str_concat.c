0d294f43df1b59ada28296dea61cd3994dc493dc
juanivalle <juanignaciovalle18@gmail.com>
2022-10-20 18:04:20 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,20 +12,20 @@ char *str_concat(char *s1, char *s2)
 	unsigned int a;
 	unsigned int b;
 
-	s3 = malloc(sizeof(char) * strlen(s1) + 1);
-	s4 = malloc(sizeof(char) * strlen(s2));
-	if (s3 == NULL)
+	if (s1 == NULL)
 	{
-		return (s4);
+		return (s2);
 	}
-	else if (s4 == NULL)
+	else if (s2 == NULL)
 	{
-		return (s3);
+		return (s1);
 	}
-	else if (s3 == NULL && s4 == NULL)
+	else if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
+	s3 = malloc(sizeof(char) * strlen(s1) + 1);
+	s4 = malloc(sizeof(char) * strlen(s2));
 	if (s3 != NULL && s4 != NULL)
 	{
 		for (a = 0; a <= strlen(s1) + 1; a++)
