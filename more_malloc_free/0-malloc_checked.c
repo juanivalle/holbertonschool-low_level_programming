39cf73a0bcfb1ddda79099a581621caf8b42d4a3
juanivalle <juanignaciovalle18@gmail.com>
2022-10-24 16:00:47 -0700

    task0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *malloc_checked - task0
+ * @b: unsigned int
+ * Return: always 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int **a;
+	unsigned int *c;
+
+	c = malloc(sizeof(int) * b);
+	a = &c;
+	if (a == NULL)
+		return (0);
+	else
+		return (a);
+}
