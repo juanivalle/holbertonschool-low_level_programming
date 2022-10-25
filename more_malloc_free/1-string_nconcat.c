97c3f126f61107035ea894ae6f55a0e00a6784d6
juanivalle <juanignaciovalle18@gmail.com>
2022-10-25 06:24:44 -0700

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,12 +27,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		if (n < strlen(s2))
 		{
-		for (a = 0; a <= strlen(s1) + 1; a++)
-			s3[a] = s1[a];
-		for (b = 0; b <= n; b++)
-			s4[b] = s2[b];
+			for (a = 0; a <= strlen(s1) + 1; a++)
+				s3[a] = s1[a];
+			for (b = 0; b <= n; b++)
+				s4[b] = s2[b];
 		}
-		else if (n > strlen(s2))
+		if (n > strlen(s2))
 		{
 			n = strlen(s2);
 		s3 = strcat(s3, s4);
