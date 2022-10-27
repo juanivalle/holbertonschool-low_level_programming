edd8fac72a3cd7daa615e55f12a31da469af8db4
juanivalle <juanignaciovalle18@gmail.com>
2022-10-27 07:53:07 -0700

    task5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - task5
+ * @d: char
+ * Return: always 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
