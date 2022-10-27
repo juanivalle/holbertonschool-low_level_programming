57d642fe2a0a0c2ef2e49be38a4d72925003512b
juanivalle <juanignaciovalle18@gmail.com>
2022-10-27 06:10:03 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#include <stdio.h>
+/**
+ *struct dog - task0
+ *@name:char
+ *@age:float
+ *@owner:char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
