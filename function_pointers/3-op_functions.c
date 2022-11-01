84904f8da4c0b90a5e8e688d2576bab0bbde65ba
juanivalle <juanignaciovalle18@gmail.com>
2022-11-01 11:55:42 -0700

    task3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - add
+ *@a: a
+ *@b: b
+ *Return: always 0
+**/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - sub
+ *@b: int
+ *@a: a
+ *Return: always 0
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul
+ *@b: int
+ *@a: a
+ *Return: always 0
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ *@b: int
+ *@a: a
+ *Return: always 0
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod
+ *@b: int
+ *@a: a
+ *Return: always 0
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
