5a124c13e39864ca7f4976dc5461219f8c1d7b04
juanivalle <juanignaciovalle18@gmail.com>
2022-11-01 11:40:11 -0700

    dogh

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,8 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-#include <stdio.h>
+#include <stdio.hu>
+#include <stdlib.h>
 /**
  *struct dog - task0
  *@name:char
@@ -19,4 +20,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 #endif
