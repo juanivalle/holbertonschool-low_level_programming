19b463234042533eda05426e82a114d6e31c3fa7
juanivalle <juanignaciovalle18@gmail.com>
2022-11-15 11:53:41 -0800

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - function that print the elements of the list
+ * @h: const list_t
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
