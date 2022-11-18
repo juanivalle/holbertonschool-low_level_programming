c9f17ac7dccc6596777914d73580bb326827eb79
juanivalle <juanignaciovalle18@gmail.com>
2022-11-18 04:05:32 -0800

    task1

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * listint_len - returns the elements at the list.
- * @h const listint_t
+ * @h: const listint_t
  * Return: The number of elements in the list.
 **/
 size_t listint_len(const listint_t *h)
