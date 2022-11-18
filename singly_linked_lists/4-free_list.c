13f543e86fdb00749eaba5c4402e8c9e879b42d6
juanivalle <juanignaciovalle18@gmail.com>
2022-11-18 03:28:40 -0800

    task4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - Frees a list.
+ * @head: A pointer to the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *fr;
+
+	while (head)
+	{
+		fr = head->next;
+		free(head->str);
+		free(head);
+		head = fr;
+	}
+}
