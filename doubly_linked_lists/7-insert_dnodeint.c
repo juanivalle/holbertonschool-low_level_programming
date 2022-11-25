4b12edcf10a3638c83b19dc0be5429f8f37a2bab
juanivalle <juanignaciovalle18@gmail.com>
2022-11-25 07:25:47 -0800

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		cop = cop->next;
 	}
 	new->next = cop->next;
+	if (cop != NULL)
+	{
+		while (cop->prev != NULL)
+			cop = cop->prev;
+	}
 	cop->next = new;
 	return (new);
 }
